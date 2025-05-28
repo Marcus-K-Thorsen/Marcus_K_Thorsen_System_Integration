468b4c24f301370d7feaf25db273925e2fdd773d
Marcus <marc303i@stud.kea.dk>
2025-05-28 15:31:02 +0200

    Made the printing of the github webhooks look better in the python server

--- 25._GitHub_Webhooks/02._python/main.py
+++ 25._GitHub_Webhooks/02._python/main.py
@@ -7,7 +7,8 @@ app = FastAPI()
 @app.post("/githubwebhookjson")
 async def github_webhook(request: Request):
     data = await request.body()
-    print(json.loads(data))
+    parsed = json.loads(data)
+    print(json.dumps(parsed, indent=2))  # Pretty print JSON
     return
 
 
@@ -16,7 +17,8 @@ async def github_webhook(request: Request, response: Response):
     if request.headers.get("content-type") == "application/x-www-form-urlencoded":
         form_data = await request.form()
         payload = form_data['payload']
-        print(payload)
+        parsed = json.loads(payload)
+        print(json.dumps(parsed, indent=2))  # Pretty print JSON
         response.status_code = 200
     else: 
         response.status_code = 400
