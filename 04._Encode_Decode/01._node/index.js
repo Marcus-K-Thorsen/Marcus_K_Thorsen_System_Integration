e51b290fe33e8cb0b46212c65b02ed1727a8fed4
Marcus <marc303i@stud.kea.dk>
2025-05-30 10:18:45 +0200

    Added more ways to encode and decode a message in my node index file

--- 04._Encode_Decode/01._node/index.js
+++ 04._Encode_Decode/01._node/index.js
@@ -1,8 +1,33 @@
 const message = "hello world";
 
-const encoded = btoa(message);
+console.log("..................................................");
 
-const decoded = atob(encoded);
+const base64Encoded = btoa(message);
 
-console.log(encoded);
-console.log(decoded);
+const base64Decoded = atob(base64Encoded);
+
+console.log("Base64 encoded message:", base64Encoded);
+console.log("Bas64 decoded message:", base64Decoded);
+
+console.log("..................................................");
+
+// Encode
+const hexEncoded = Buffer.from(message, 'utf8').toString('hex');
+// Decode
+const hexDecoded = Buffer.from(hexEncoded, 'hex').toString('utf8');
+
+
+console.log("Hex encoded message:", hexEncoded);
+console.log("Hex decoded message:", hexDecoded);
+
+console.log("..................................................");
+
+// Encode
+const uriEncoded = encodeURIComponent(message);
+// Decode
+const uriDecoded = decodeURIComponent(uriEncoded);
+
+console.log("URI encoded message:", uriEncoded);
+console.log("URI decoded message:", uriDecoded);
+
+console.log("..................................................");
